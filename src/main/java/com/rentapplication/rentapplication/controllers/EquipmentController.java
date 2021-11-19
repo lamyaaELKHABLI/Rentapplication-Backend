3a6058bc711f6bbce4af92f628625dfb5bad0a84
Lamyaa EL KHABLI <88164221+lamyaaELKHABLI@users.noreply.github.com>
2021-11-19 01:10:11 +0100

    Update EquipmentController.java

--- src/main/java/com/rentapplication/rentapplication/controllers/EquipmentController.java
+++ src/main/java/com/rentapplication/rentapplication/controllers/EquipmentController.java
@@ -33,9 +33,9 @@ public class EquipmentController {
         return equipmentrepository.findAll();
     }
 
-    @CrossOrigin("http://localhost:4200")
+   /* @CrossOrigin("http://localhost:4200")
     @PostMapping(value = "/savelogementequipment")
-    public void SaveLogementEquipment (@RequestParam("equipment_id") Integer equipment_id,
+    public LogementCategoryRepository SaveLogementEquipment (@RequestParam("equipment_id") Integer equipment_id,
                                 @RequestParam("logement_id") Integer logement_id
     ) throws JsonParseException, JsonMappingException, Exception{
         Logement newlogement = logementrepository.findById(logement_id).orElse(null);
@@ -44,5 +44,5 @@ public class EquipmentController {
         equipmentlogement.setEquipment(newequipment);
         equipmentlogement.setLogement(newlogement);
     return logementcategoryrepository.save(equipmentlogement);
-    }
+    }*/
 }
